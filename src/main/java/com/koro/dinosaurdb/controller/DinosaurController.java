d2097623b6142adb17decefeed56dde4557abc86
conboaduide <anhvnvse172006@fpt.edu.vn>
2023-11-01 02:48:11 +0700

    add crud dinosaur.

--- src/main/java/com/koro/dinosaurdb/controller/DinosaurController.java
+++ src/main/java/com/koro/dinosaurdb/controller/DinosaurController.java
@@ -1,10 +1,9 @@
 package com.koro.dinosaurdb.controller;
 
+import com.koro.dinosaurdb.entity.Dinosaur;
 import com.koro.dinosaurdb.entity.ResponseObject;
 import com.koro.dinosaurdb.service.DinosaurService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/dinosaurs")
@@ -19,5 +18,20 @@ public class DinosaurController {
     @GetMapping
     public ResponseObject getDinosaurs() {
         return dinosaurService.getDinosaurs();
+    }
+
+    @PostMapping
+    public ResponseObject createDinosaur(@RequestBody Dinosaur dinosaur) {
+        return dinosaurService.createDinosaur(dinosaur);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseObject deleteDinosaur(@PathVariable String id){
+        return dinosaurService.deleteDinosaur(id);
+    }
+
+    @PatchMapping ("/{id}")
+    public ResponseObject updateDinosaur(@PathVariable String id, @RequestBody Dinosaur dinosaur){
+        return dinosaurService.updateDinosaur(id, dinosaur);
     }
 }
--- src/main/java/com/koro/dinosaurdb/service/DinosaurService.java
+++ src/main/java/com/koro/dinosaurdb/service/DinosaurService.java
@@ -1,5 +1,6 @@
 package com.koro.dinosaurdb.service;
 
+import com.koro.dinosaurdb.entity.Dinosaur;
 import com.koro.dinosaurdb.entity.DinosaurList;
 import com.koro.dinosaurdb.entity.ResponseObject;
 import org.springframework.stereotype.Component;
@@ -9,5 +10,34 @@ public class DinosaurService {
 
     public ResponseObject getDinosaurs() {
         return new ResponseObject("Ok", "Success", DinosaurList.getInstance());
+    }
+
+    public ResponseObject createDinosaur(Dinosaur dinosaur) {
+        DinosaurList.getInstance().add(dinosaur);
+        return new ResponseObject("Ok", "Success", DinosaurList.getInstance().get(DinosaurList.getInstance().size()-1));
+    }
+
+    public ResponseObject deleteDinosaur(String id) {
+        for (Dinosaur dinosaur : DinosaurList.getInstance()){
+            if(dinosaur.getId().equals(id)){
+                DinosaurList.getInstance().remove(dinosaur);
+                return new ResponseObject("Ok", "Success", id);
+            }
+        }
+        return new ResponseObject("Fail", "Not Found", id);
+    }
+
+    public ResponseObject updateDinosaur(String id, Dinosaur dinosaur) {
+        for (Dinosaur d : DinosaurList.getInstance()){
+            if (d.getId().equals(id)) {
+                d.setSpecies(dinosaur.getSpecies() != null ? dinosaur.getSpecies() : d.getSpecies());
+                d.setHabitat(dinosaur.getHabitat() != null ? dinosaur.getHabitat() : d.getHabitat());
+                d.setLocation(dinosaur.getLocation() != null ? dinosaur.getLocation() : d.getLocation());
+                d.setLivingPeriod(dinosaur.getLivingPeriod() != null ? dinosaur.getLivingPeriod() : d.getLivingPeriod());
+                d.setDiscoveredYear(dinosaur.getDiscoveredYear() != 0 ? dinosaur.getDiscoveredYear() : d.getDiscoveredYear());
+                return new ResponseObject("Ok", "Success", id);
+            }
+        }
+        return new ResponseObject("Fail", "Not Found", id);
     }
 }
